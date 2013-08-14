c67988dca803b6ed79fe6df512897c7f7e0dd281
Johan Holmberg <johan@idioti.se>
2013-08-14 16:56:10 +0200

    Managed to forget to add files to last commit...

---
+++ building api/src/main/java/se/mah/elis/adaptor/building/api/data/DeviceSetIdentifier.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package se.mah.elis.adaptor.building.api.data;
+
+/**
+ * The DeviceSetIdentifier interface is used to address a device set.
+ * 
+ * @author "Johan Holmberg, Malmö University"
+ * @since 1.0
+ */
+public interface DeviceSetIdentifier {
+
+}
