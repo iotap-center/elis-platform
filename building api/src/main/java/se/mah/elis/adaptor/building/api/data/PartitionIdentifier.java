27023ea3c84f76d69860bf6c70b5516c2468a85e
Johan Holmberg <johan@idioti.se>
2013-08-14 16:56:10 +0200

    Managed to forget to add files to last commit...

---
+++ building api/src/main/java/se/mah/elis/adaptor/building/api/data/PartitionIdentifier.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package se.mah.elis.adaptor.building.api.data;
+
+/**
+ * The PartitionIdentifer interface is used to address a partition of a
+ * building.
+ * 
+ * @author "Johan Holmberg, Malmö University"
+ * @since 1.0
+ */
+public interface PartitionIdentifier {
+
+}
---
+++ building api/src/main/java/se/mah/elis/adaptor/building/api/exceptions/MethodNotSupportedException.java
@@ -0,0 +1,18 @@
+package se.mah.elis.adaptor.building.api.exceptions;
+
+/**
+ * Thrown when a method isn't supported by the underlying platform.
+ * 
+ * @version 1.0
+ * @author "Johan Holmberg, Malmö University"
+ */
+public class MethodNotSupportedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6552229052231609208L;
+
+
+
+}
