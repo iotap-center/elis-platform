586652c96c733b481d036ca75f1273cfeb6e126f
Marcus Ljungblad <marcus@ljungblad.nu>
2013-08-29 13:13:55 +0200

    implementation of gateway.connect()

--- eon-utility-provider/src/main/java/se/mah/elis/adaptor/utilityprovider/eon/internal/gateway/EonGateway.java
+++ eon-utility-provider/src/main/java/se/mah/elis/adaptor/utilityprovider/eon/internal/gateway/EonGateway.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.client.ResponseProcessingException;
+
+import org.json.simple.parser.ParseException;
 
 import se.mah.elis.adaptor.building.api.data.GatewayAddress;
 import se.mah.elis.adaptor.building.api.entities.GatewayUser;
@@ -11,23 +16,29 @@ import se.mah.elis.adaptor.building.api.entities.devices.Device;
 import se.mah.elis.adaptor.building.api.entities.devices.Gateway;
 import se.mah.elis.adaptor.building.api.exceptions.GatewayCommunicationException;
 import se.mah.elis.adaptor.building.api.exceptions.StaticEntityException;
+import se.mah.elis.adaptor.building.api.exceptions.MethodNotSupportedException;
 import se.mah.elis.adaptor.utilityprovider.eon.internal.EonHttpBridge;
+import se.mah.elis.adaptor.utilityprovider.eon.internal.devices.EonDeviceIdentifier;
+import se.mah.elis.adaptor.utilityprovider.eon.internal.devices.EonPowerSwitchMeter;
 
 public class EonGateway implements Gateway {
 
-	private String authenticationToken; 
+	private String authenticationToken;
 	private EonHttpBridge httpBridge;
 	private List<Device> devices;
 	private String name;
 	private GatewayAddress gatewayAddress;
-	private GatewayUser gatewayUser; 
-	
+	private GatewayUser gatewayUser;
+	private boolean gatewayHasConnected;
+
 	public EonGateway() {
 		this.devices = new ArrayList<Device>();
+		this.gatewayHasConnected = false;
 	}
-	
+
 	/**
 	 * Get the active E.On authentication token
+	 * 
 	 * @return
 	 */
 	public String getAuthenticationToken() {
@@ -36,6 +47,7 @@ public class EonGateway implements Gateway {
 
 	/**
 	 * Set active E.On authentication token
+	 * 
 	 * @param authenticationToken
 	 */
 	public void setAuthenticationToken(String authenticationToken) {
@@ -158,18 +170,77 @@ public class EonGateway implements Gateway {
 	}
 
 	/**
-	 * Populates the gateway with all known devices and maintains them 
-	 * in memory. Upon success this will cause hasConnected() to return true. 
+	 * Populates the gateway with all known devices and maintains them in
+	 * memory. Upon success this will cause hasConnected() to return true.
 	 */
 	@Override
 	public void connect() throws GatewayCommunicationException {
+		try {
+			addGatewayData();
+			getAllDevices();
+			markAsConnected();
+		} catch (Exception e) {
+			throw new GatewayCommunicationException();
+		}
+	}
+
+	private void addGatewayData() throws ResponseProcessingException,
+			ParseException, StaticEntityException {
+		Map<String, Object> data = httpBridge
+				.getGateway(getAuthenticationToken());
+		setName((String) data.get("Name"));
+		setId(Integer.parseInt((String) data.get("EwpPanelId")));
+	}
+
+	private void getAllDevices() throws ResponseProcessingException,
+			ParseException {
 		
+		List<Map<String, Object>> data = httpBridge.getDevices(
+				getAuthenticationToken(), getAddress().toString());
+		
+		for (Map<String, Object> deviceMap : data) {
+			try {
+				Device device = convertToDevice(deviceMap);
+				this.add(device);
+			} catch (MethodNotSupportedException e) {
+				e.printStackTrace();
+				continue; // TODO: add logging
+			} catch (StaticEntityException e) {
+				e.printStackTrace();
+				continue; // TODO: add logging
+			}
+		}
+	}
+
+	private Device convertToDevice(Map<String, Object> deviceMap)
+			throws MethodNotSupportedException, StaticEntityException {
+		
+		if ((boolean) deviceMap.get("IsPowerSwitch")) {
+			return createPowerSwitch(deviceMap);
+		} else {
+			throw new MethodNotSupportedException();
+		}
+	}
+
+	private Device createPowerSwitch(Map<String, Object> deviceMap) throws StaticEntityException {
+		EonPowerSwitchMeter psm = new EonPowerSwitchMeter();
+		String id = (String) deviceMap.get("Id");
+		
+		psm.setGateway(this);
+		psm.setHttpBridge(getHttpBridge());
+		psm.setId(new EonDeviceIdentifier(id));
+		psm.setName((String) deviceMap.get("Name"));
+		
+		return psm;
+	}
+
+	private void markAsConnected() {
+		this.gatewayHasConnected = true;
 	}
 
 	@Override
 	public boolean hasConnected() {
-		// TODO Auto-generated method stub
-		return false;
+		return gatewayHasConnected;
 	}
 
 	@Override
--- eon-utility-provider/src/test/java/se/mah/elis/adaptor/utilityprovider/eon/test/EonGatewayTest.java
+++ eon-utility-provider/src/test/java/se/mah/elis/adaptor/utilityprovider/eon/test/EonGatewayTest.java
@@ -5,7 +5,9 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.naming.AuthenticationException;
@@ -20,7 +22,7 @@ import se.mah.elis.adaptor.utilityprovider.eon.internal.EonHttpBridge;
 import se.mah.elis.adaptor.utilityprovider.eon.internal.gateway.EonGateway;
 
 public class EonGatewayTest {
-	
+
 	private static final String TEST_GATEWAY_NAME = "testGateway";
 	private static final String TEST_TOKEN = "sometoken";
 	private static final String TEST_GATEWAY_ADDRESS = "someaddress";
@@ -28,20 +30,34 @@ public class EonGatewayTest {
 	private EonGateway gateway;
 
 	@Before
-	public void setUp() throws AuthenticationException, ResponseProcessingException, ParseException {
+	public void setUp() throws AuthenticationException,
+			ResponseProcessingException, ParseException {
 		bridge = mock(EonHttpBridge.class);
-		when(bridge.authenticate(anyString(), anyString())).thenReturn(TEST_TOKEN);
+		when(bridge.authenticate(anyString(), anyString())).thenReturn(
+				TEST_TOKEN);
 		when(bridge.getGateway(TEST_TOKEN)).thenReturn(fakeGatewayData());
-		
+		when(bridge.getDevices(TEST_TOKEN, TEST_GATEWAY_ADDRESS)).thenReturn(
+				fakeDeviceList());
+
 		GatewayAddress address = mock(GatewayAddress.class);
 		when(address.toString()).thenReturn(TEST_GATEWAY_ADDRESS);
-		
+
 		gateway = new EonGateway();
 		gateway.setAuthenticationToken(TEST_TOKEN);
 		gateway.setHttpBridge(bridge);
 		gateway.setAddress(address);
 	}
-	
+
+	private List<Map<String, Object>> fakeDeviceList() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		Map<String, Object> device = new HashMap<String, Object>();
+		device.put("Id", "someid");
+		device.put("Name", "testDevice");
+		device.put("IsPowerSwitch", true);
+		list.add(device);
+		return list;
+	}
+
 	private Map<String, Object> fakeGatewayData() {
 		Map<String, Object> fakeGatewayData = new HashMap<String, Object>();
 		fakeGatewayData.put("Name", TEST_GATEWAY_NAME);
@@ -49,12 +65,13 @@ public class EonGatewayTest {
 		return fakeGatewayData;
 	}
 
-	@Test 
+	@Test
 	public void testConnect() {
 		try {
 			gateway.connect();
-			assertTrue(gateway.hasConnected());
 			assertEquals(TEST_GATEWAY_NAME, gateway.getName());
+			assertTrue(gateway.size() > 0);
+			assertTrue(gateway.hasConnected());
 		} catch (Exception e) {
 			fail();
 		}
