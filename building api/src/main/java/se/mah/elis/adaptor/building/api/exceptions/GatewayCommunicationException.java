09895b377c7166db2b35d20767c1ef13c34ee080
Johan Holmberg <johan@idioti.se>
2013-09-02 13:53:50 +0200

    Updated the GatewayCommunicationException with functionality for saving and retrieving an originating exception.

--- building api/src/main/java/se/mah/elis/adaptor/building/api/exceptions/GatewayCommunicationException.java
+++ building api/src/main/java/se/mah/elis/adaptor/building/api/exceptions/GatewayCommunicationException.java
@@ -12,9 +12,39 @@ package se.mah.elis.adaptor.building.api.exceptions;
  */
 public class GatewayCommunicationException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -4745221381027469389L;
-
+	
+	private Exception original;
+	
+	/**
+	 * Creates an instance of this exception.
+	 * 
+	 * @since 1.0
+	 */
+	public GatewayCommunicationException() {
+		super();
+		original = null;
+	}
+	
+	/**
+	 * Creates an instance of this exception, with another exception being
+	 * saved as a reference to what originally triggered this exception.
+	 * 
+	 * @param origin The originating exception.
+	 * @since 1.0
+	 */
+	public GatewayCommunicationException(Exception origin) {
+		super();
+		original = origin;
+	}
+	
+	/**
+	 * Returns the exception that originally raised this exception.
+	 * 
+	 * @return The originating exception.
+	 * @since 1.0
+	 */
+	public Exception getOriginalException() {
+		return original;
+	}
 }
