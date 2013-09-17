da9f748b1914dfe360798adebc3eb1d4661f7600
Johan Holmberg <johan@idioti.se>
2013-09-17 13:41:45 +0200

    Added a more sane sanity check.

--- Elis user service implementation/src/main/java/se/mah/elis/services/users/impl/PlatformUserIdentifier.java
+++ Elis user service implementation/src/main/java/se/mah/elis/services/users/impl/PlatformUserIdentifier.java
@@ -49,7 +49,7 @@ public class PlatformUserIdentifier implements UserIdentifier {
 	}
 
 	public void setUsername(String username) throws IllegalArgumentException {
-		if (username.length() > 0) {
+		if (!username.isEmpty()) {
 			this.username = username;
 		} else {
 			throw new IllegalArgumentException("User name can't be empty");
@@ -65,7 +65,7 @@ public class PlatformUserIdentifier implements UserIdentifier {
 	}
 
 	public void setPassword(String password) {
-		if (password.length() > 0) {
+		if (!password.isEmpty()) {
 			this.password = password;
 		} else {
 			throw new IllegalArgumentException("Password can't be empty");
