2f7e1a79cd8a87474b657d2f99825966d19bcf2c
Marcus Ljungblad <marcus@ljungblad.nu>
2013-09-18 15:33:46 +0200

    authenticate implemented

--- oauth2-service/src/main/java/se/mah/elis/external/web/oauth/OAuthResource.java
+++ oauth2-service/src/main/java/se/mah/elis/external/web/oauth/OAuthResource.java
@@ -1,20 +1,99 @@
 package se.mah.elis.external.web.oauth;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import se.mah.elis.authentication.oauth.OAuthService;
+
+/**
+ * Elis HTTP end-point for OAuth authentication for clients and applications
+ * 
+ * @author Marcus Ljungblad
+ * @version 1.0.0
+ * @since 1.0
+ */
 @Path("/oauth2")
 @Produces("application/json")
 @Consumes("application/json")
 public class OAuthResource {
 
+	// error envelopes 
+	private static final String invalidClientId = ""
+			+ "elisApi({\n"
+			+ "  \"status\": \"ERROR\",\n"
+			+ "  \"code\": \"400\",\n"
+			+ "  \"errorType\": \"invalid client id\",\n"
+			+ "  \"errorDetail\": \"Client ID is empty, incorrectly formatted or does not exist.\","
+			+ "  \"response\": {}"
+			+ "})";
+	private static final String invalidRedirectURI = ""
+			+ "elisApi({\n"
+			+ "  \"status\": \"ERROR\",\n"
+			+ "  \"code\": \"400\",\n"
+			+ "  \"errorType\": \"invalid redirect uri\",\n"
+			+ "  \"errorDetail\": \"Redirect URI is empty or incorrectly formatted.\","
+			+ "  \"response\": {}"
+			+ "})";
+	
+	// internals
+	private OAuthService oauthService;
+
+	public OAuthResource(OAuthService oauthService) {
+		this.oauthService = oauthService;
+	}
+	
+	public void setOAuthService(OAuthService oauthService) {
+		this.oauthService = oauthService;
+	}
+	
+	@GET
 	@Path("/authenticate")
-	public Response authenticate(String clientId, String redirectUri) {
-		return null;
+	public Response authenticate(
+			@DefaultValue("") @QueryParam("client_id") String clientId, 
+			@DefaultValue("") @QueryParam("redirect_uri") String redirectUri) {
+		Response response = null;
+		
+		if (!clientId.isEmpty() && !redirectUri.isEmpty())
+			response = handle_authenticate(clientId, redirectUri);
+		else {
+			if (clientId.isEmpty())
+				response = invalidClientId();
+			else if (redirectUri.isEmpty()) 
+				response = invalidRedirectUri();
+		}
+		
+		return response;
 	}
 
+	private Response handle_authenticate(String clientId, String redirectUri) {
+		Response response = Response.status(Status.FOUND)
+				.header("Elis-Authorization-Code", oauthService.createAuthorizationCode())
+				.header("Location", redirectUri)
+				.build();
+		return response;
+	}
+
+	private Response invalidRedirectUri() {
+		Response response = Response.status(Status.BAD_REQUEST)
+				.entity(invalidRedirectURI)
+				.build();
+		return response;
+	}
+
+	private Response invalidClientId() {
+		Response response = Response.status(Status.BAD_REQUEST)
+				.entity(invalidClientId)
+				.build();
+		return response;
+	}
+
+	@GET
 	@Path("/access_token")
 	public Response accessToken(String clientId,
 			String clientAuthorizationCode, String redirectUri) {
--- oauth2-service/src/test/java/se/mah/elis/external/web/oauth/test/OAuthResourceTest.java
+++ oauth2-service/src/test/java/se/mah/elis/external/web/oauth/test/OAuthResourceTest.java
@@ -3,6 +3,10 @@ package se.mah.elis.external.web.oauth.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -10,6 +14,7 @@ import javax.ws.rs.core.Response.Status;
 import org.junit.Before;
 import org.junit.Test;
 
+import se.mah.elis.authentication.oauth.OAuthService;
 import se.mah.elis.external.web.oauth.OAuthResource;
 
 public class OAuthResourceTest {
@@ -17,35 +22,46 @@ public class OAuthResourceTest {
 	// test variables
 	private static final String TEST_CLIENTID = "batman";
 	private static final String TEST_REDIRECT_URI = "http://batcave.mah.se/redirect.php";
+	private static final String TEST_AUTHCODE = "ec0e2603172c73a8b644bb9456c1ff6e";
+	private static final String TEST_ACCESSCODE = "8ee60a2e00c90d7e00d5069188dc115b";
 	
 	// internals
 	private OAuthResource oauth;
 	
 	@Before
 	public void setUp() {
-		oauth = new OAuthResource();
+		OAuthService oauthService = mock(OAuthService.class);
+		when(oauthService.createAuthorizationCode()).thenReturn(TEST_AUTHCODE);
+		when(oauthService.createAccessToken()).thenReturn(TEST_ACCESSCODE);
+		oauth = new OAuthResource(oauthService);
 	}
 	
 	@Test
 	public void testAuthenticateSuccess() {
 		Response response = oauth.authenticate(TEST_CLIENTID, TEST_REDIRECT_URI);
-		assertEquals(Status.FOUND, response.getStatus());
+		assertEquals(Status.FOUND, Status.fromStatusCode(response.getStatus()));
 		
 		String authCode = response.getHeaderString("Elis-Authorization-Code");
 		String redirectTo = response.getHeaderString("Location");
 		
 		assertNotNull(authCode);
 		assertTrue(!authCode.isEmpty());
+		assertEquals(TEST_AUTHCODE, authCode);
 		
 		assertNotNull(redirectTo);
 		assertEquals(TEST_REDIRECT_URI, redirectTo);
 	}
 	
+	@Test
+	public void testOAuthServiceNotAvailableOnSuccessRequest() {
+		fail();
+	}
+	
 	@Test 
 	public void testAuthenticateClientIdInvalid() {
 		Response response = oauth.authenticate("", TEST_REDIRECT_URI);
-		assertEquals(Status.BAD_REQUEST , response.getStatus());
-		String body = response.readEntity(String.class);
+		assertEquals(Status.BAD_REQUEST, Status.fromStatusCode(response.getStatus()));
+		String body = (String) response.getEntity();
 		String expectedBody = ""
 				+ "elisApi({\n"
 				+ "  \"status\": \"ERROR\",\n"
@@ -60,8 +76,8 @@ public class OAuthResourceTest {
 	@Test 
 	public void testAuthenticateInvalidRedirectURI() {
 		Response response = oauth.authenticate(TEST_CLIENTID, "");
-		assertEquals(Status.BAD_REQUEST , response.getStatus());
-		String body = response.readEntity(String.class);
+		assertEquals(Status.BAD_REQUEST, Status.fromStatusCode(response.getStatus()));
+		String body = (String) response.getEntity();
 		String expectedBody = ""
 				+ "elisApi({\n"
 				+ "  \"status\": \"ERROR\",\n"
@@ -75,26 +91,26 @@ public class OAuthResourceTest {
 	
 	@Test
 	public void testAuthenticateMissingParams() {
-		
+		fail();
 	}
 	
 	@Test
 	public void testAccessTokenSuccess() {
-		
+		fail();
 	}
 	
 	@Test
 	public void testAccessTokenMissingParams() {
-		
+		fail();
 	}
 	
 	@Test
 	public void testAccessTokenMismatchClientIdAndClientSecret() {
-		
+		fail();
 	}
 	
 	@Test
 	public void testAccessTokenInvalidRedirectURI() {
-		
+		fail();
 	}
 }
