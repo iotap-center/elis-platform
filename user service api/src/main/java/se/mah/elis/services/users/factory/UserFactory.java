f700bb6f3156a1f4d084ce3f943a52904f1dfa0e
Johan Holmberg <johan@idioti.se>
2013-09-19 14:21:24 +0200

    Added interfaces describing a user factory.

---
+++ user service api/src/main/java/se/mah/elis/services/users/factory/UserFactory.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package se.mah.elis.services.users.factory;
+
+import java.util.Properties;
+
+import se.mah.elis.services.users.User;
+import se.mah.elis.services.users.exceptions.UserInitalizationException;
+
+/**
+ * The UserFactory builds users. A UserProvider can register with a UserFactory
+ * so that the factory can produce the user types provided by the UserProvider.
+ * 
+ * @author "Johan Holmberg, Malmö University"
+ * @since 1.0
+ */
+public interface UserFactory {
+	
+	/**
+	 * Registers a user provider with this factory.
+	 * 
+	 * @param provider The UserProvider to register.
+	 * @since 1.0
+	 */
+	public void registerProvider(UserProvider provider);
+	
+	/**
+	 * Unregisters a user provider from this factory.
+	 * 
+	 * @param provider The UserProvider to unregister.
+	 * @since 1.0
+	 */
+	public void unregisterProvider(UserProvider provider);
+	
+	/**
+	 * Builds a user. As a user type can be provided by several different
+	 * supporting services, we must also provide the name of the service that
+	 * the user should target.
+	 * 
+	 * @param userType The user type that we want to build. This should match
+	 * 		  the name of the implemented User interface.
+	 * @param serviceName The name of the service that we want to target.
+	 * @param properties The properties of the new user, as specified by its
+	 * 		  UserRecipe.
+	 * @return The newly built User object.
+	 * @throws UserInitalizationException if the user couldn't be built for
+	 * 		   whatever reason.
+	 * @since 1.0
+	 */
+	public User build(String userType, String serviceName, Properties properties)
+			throws UserInitalizationException;
+	
+	/**
+	 * Returns a list of all available user recipes.
+	 * 
+	 * @return An array of user recipes. If no user recipes are available, the
+	 * 		   method returns an empty array.
+	 * @since 1.0
+	 */
+	public UserRecipe[] getAvailableUserRecipes();
+}
---
+++ user service api/src/main/java/se/mah/elis/services/users/factory/UserRecipe.java
@@ -0,0 +1,38 @@
+package se.mah.elis.services.users.factory;
+
+import java.util.Properties;
+
+/**
+ * The UserRecipe interface describes a way to build a user, using a
+ * UserFactory.
+ * 
+ * @author "Johan Holmberg, Malmö University"
+ * @since 1.0
+ */
+public interface UserRecipe {
+	
+	/**
+	 * Returns the user type description. This should match the name of the
+	 * User interface being implemented.
+	 * 
+	 * @return The name of the user type.
+	 * @since 1.0
+	 */
+	public String getUserType();
+	
+	/**
+	 * Returns the service name that provides this user type.
+	 * 
+	 * @return The name of the providing service.
+	 * @since 1.0
+	 */
+	public String getServiceName();
+	
+	/**
+	 * Returns a list of properties needed to successfully build a user.
+	 * 
+	 * @return A list of properties.
+	 * @since 1.0
+	 */
+	public Properties getProperties();
+}
