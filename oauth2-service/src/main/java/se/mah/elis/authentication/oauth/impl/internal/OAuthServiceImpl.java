d4f09287d722554718861aa80f5aeafa4a9415e2
Marcus Ljungblad <marcus@ljungblad.nu>
2013-09-19 17:04:50 +0200

    placeholders for oauth service implementation and tests

---
+++ oauth2-service/src/main/java/se/mah/elis/authentication/oauth/impl/internal/OAuthServiceImpl.java
@@ -0,0 +1,43 @@
+package se.mah.elis.authentication.oauth.impl.internal;
+
+import se.mah.elis.authentication.oauth.OAuthService;
+
+public class OAuthServiceImpl implements OAuthService {
+
+	@Override
+	public String createAccessToken(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createAuthorizationCode(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createRefreshToken(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean verifyAccessToken(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean verifyAuthorizationCode(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean verifyAuthorizationCode(String arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
