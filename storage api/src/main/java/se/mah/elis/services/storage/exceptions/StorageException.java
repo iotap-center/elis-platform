7c05b886c0a37b1b14eca3e09529c396f49c757c
koddas <johan@idioti.se>
2013-11-28 22:48:54 +0100

    Added some more documentation.

--- storage api/src/main/java/se/mah/elis/services/storage/exceptions/StorageException.java
+++ storage api/src/main/java/se/mah/elis/services/storage/exceptions/StorageException.java
@@ -15,6 +15,11 @@ public class StorageException extends Exception {
 	/**
 	 * 
 	 */
+	private static final long serialVersionUID = 1907776511239444176L;
+
+	/**
+	 * 
+	 */
 	public StorageException() {
 		// TODO Auto-generated constructor stub
 	}
