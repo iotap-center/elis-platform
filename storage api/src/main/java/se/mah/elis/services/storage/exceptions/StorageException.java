0f1cd8bfa63d8d1c6e21e39404b785022af4997c
koddas <johan@idioti.se>
2014-01-15 16:54:46 +0100

    * Updated the documentation of StorageException * Added a constructor taking an error message to StorageException.

--- storage api/src/main/java/se/mah/elis/services/storage/exceptions/StorageException.java
+++ storage api/src/main/java/se/mah/elis/services/storage/exceptions/StorageException.java
@@ -3,25 +3,34 @@
  */
 package se.mah.elis.services.storage.exceptions;
 
-// TODO: Fixa dokumentationen här!
 /**
- * A StorageException is thrown. That is all
+ * A StorageException is thrown when the persistent storage fails to write,
+ * delete or retrieve an object.
  * 
  * @author "Johan Holmberg, Malmö University"
  * @since 1.0
  */
 public class StorageException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1907776511239444176L;
 
 	/**
+	 * Creates an instance of StorageException.
 	 * 
+	 * @since 1.0 
 	 */
 	public StorageException() {
-		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Creates an instance of StorageException with a short description of the
+	 * nature of the error.
+	 * 
+	 * @param message The error message.
+	 * @since 1.1
+	 */
+	public StorageException(String message) {
+		super(message);
 	}
 
 }
