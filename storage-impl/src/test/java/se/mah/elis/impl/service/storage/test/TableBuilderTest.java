003c549bf869d357a975e22846e0b65825adbbf3
koddas <johan@idioti.se>
2014-01-24 14:03:09 +0100

    Added test files for the persistent storage implementation.

---
+++ storage-impl/src/test/java/se/mah/elis/impl/service/storage/test/TableBuilderTest.java
@@ -0,0 +1,24 @@
+package se.mah.elis.impl.service.storage.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TableBuilderTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ storage-impl/src/test/java/se/mah/elis/impl/services/storage/result/test/ResultSetImplTest.java
@@ -0,0 +1,24 @@
+package se.mah.elis.impl.services.storage.result.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ResultSetImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
