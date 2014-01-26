7f7e2146e38eb9cdec3b4557594074ce37582f2b
koddas <johan@idioti.se>
2014-01-26 14:12:01 +0100

    Added a MockPredicate.

---
+++ storage api/src/test/java/se/mah/elis/services/storage/query/test/mock/MockPredicate.java
@@ -0,0 +1,25 @@
+package se.mah.elis.services.storage.query.test.mock;
+
+import se.mah.elis.services.storage.query.Predicate;
+import se.mah.elis.services.storage.query.QueryTranslator;
+
+public class MockPredicate implements Predicate {
+
+	private int c;
+	
+	public MockPredicate(int cardinality) {
+		c = cardinality;
+	}
+	
+	@Override
+	public String compile() {
+		return "MockPredicate " + c;
+	}
+
+	@Override
+	public void setTranslator(QueryTranslator translator) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
