f3da1285dfd319452a5778108ab80b401f0440be
Marcus Ljungblad <marcus@ljungblad.nu>
2014-01-31 13:35:00 +0100

    Updating to match new User interface

--- Elis user web service/src/test/java/se/mah/elis/services/users/impl/test/mock/MockUserIdentifier.java
+++ Elis user web service/src/test/java/se/mah/elis/services/users/impl/test/mock/MockUserIdentifier.java
@@ -1,5 +1,8 @@
 package se.mah.elis.services.users.impl.test.mock;
 
+import java.util.Properties;
+
+import se.mah.elis.data.OrderedProperties;
 import se.mah.elis.services.users.UserIdentifier;
 
 public class MockUserIdentifier implements UserIdentifier {
@@ -12,5 +15,29 @@ public class MockUserIdentifier implements UserIdentifier {
 
 	public String toString() {
 		return id;
+	}
+
+	@Override
+	public Properties getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderedProperties getPropertiesTemplate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class identifies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void identifies(Class clazz) {
+		// TODO Auto-generated method stub
+		
 	}
 }
