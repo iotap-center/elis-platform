3617be3b3a5d08408573d0eacd638e5e31400f90
Marcus Ljungblad <marcus@ljungblad.nu>
2014-02-05 15:44:15 +0100

    eon adaptor begins using proper logging instead of regular prints

--- eon-utility-provider/src/main/java/se/mah/elis/adaptor/utilityprovider/eon/EonAdaptor.java
+++ eon-utility-provider/src/main/java/se/mah/elis/adaptor/utilityprovider/eon/EonAdaptor.java
@@ -3,27 +3,30 @@ package se.mah.elis.adaptor.utilityprovider.eon;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import se.mah.elis.adaptor.device.api.providers.GatewayUserProvider;
 import se.mah.elis.adaptor.utilityprovider.eon.internal.gateway.EonGatewayUserFactory;
 
-public class Activator implements BundleActivator {
+public class EonAdaptor implements BundleActivator {
 
+	private static final Logger logger = LoggerFactory.getLogger(EonAdaptor.class); 
 	private GatewayUserProvider userFactory;
 	ServiceRegistration sr;
 	
-	public Activator() {
+	public EonAdaptor() {
 		userFactory = new EonGatewayUserFactory();
 	}
 	
 	public void start(BundleContext context) throws Exception {
-		System.out.println("Starting the E.On bundle.");
+		logger.info("The E.On bundle just started");
 		
 		sr = (ServiceRegistration) context.registerService(
 				GatewayUserProvider.class.getName(), userFactory, null);
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		System.out.println("Stopping the E.On bundle.");
+		logger.info("The E.On bundle just stopped");
 	}
 }
