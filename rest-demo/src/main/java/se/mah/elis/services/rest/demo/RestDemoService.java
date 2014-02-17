508a205555a2dc4a8260547ba0b100d1c8664ee0
Marcus Ljungblad <marcus@ljungblad.nu>
2014-02-17 14:56:22 +0100

    removing duplicate code from demo

--- rest-demo/src/main/java/se/mah/elis/services/rest/demo/RestDemoService.java
+++ rest-demo/src/main/java/se/mah/elis/services/rest/demo/RestDemoService.java
@@ -1,22 +1,15 @@
 package se.mah.elis.services.rest.demo;
 
-import java.util.Properties;
-
+import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
-import org.apache.felix.scr.annotations.Component;
 
 import se.mah.elis.adaptor.device.api.entities.GatewayUser;
 import se.mah.elis.adaptor.device.api.entities.devices.Device;
 import se.mah.elis.adaptor.device.api.entities.devices.Gateway;
-import se.mah.elis.adaptor.device.api.exceptions.GatewayCommunicationException;
 import se.mah.elis.services.users.PlatformUser;
 import se.mah.elis.services.users.User;
 import se.mah.elis.services.users.UserService;
-import se.mah.elis.services.users.exceptions.NoSuchUserException;
-import se.mah.elis.services.users.exceptions.UserExistsException;
-import se.mah.elis.services.users.exceptions.UserInitalizationException;
-import se.mah.elis.services.users.factory.UserFactory;
 
 @Component(name = "ElisHTTPDemo", immediate = true)
 @Service(value = RestDemoService.class)
@@ -27,35 +20,14 @@ public class RestDemoService {
 	@Reference
 	private UserService userService;
 	
-	@Reference
-	private UserFactory userFactory;
-	
 	public RestDemoService() {
 		populated = false;
 	}
 	
 	protected void bindUserService(UserService us) {
 		this.userService = us;
-		
-		if (!populated) 
-			populate();
 	}
 	
-	protected void bindUserFactory(UserFactory uf) {
-		this.userFactory = uf;
-		
-		if (!populated) 
-			populate();
-	}
-	
-	private void populate() {
-		if (userService == null || userFactory == null)
-			return;
-		PlatformUser pu = createMljungblad();
-		createDemoUser(pu);
-		runDemo(pu);
-	}
-
 	private void runDemo(PlatformUser pu) {
 		User[] users = userService.getUsers(pu);
 		for (User user : users) {
@@ -72,56 +44,7 @@ public class RestDemoService {
 		}
 	}
 
-	private void createDemoUser(PlatformUser pu) {
-		User eonUser = createEonUser();
-		
-		try {
-			userService.registerUserToPlatformUser(eonUser, pu);
-		} catch (NoSuchUserException e) {
-			e.printStackTrace();
-		}
-		
-		populated = true;
-		
-		System.out.println("Created a demo user called mljungblad/elis");
-	}
-
-	private User createEonUser() {
-		Properties props = new Properties();
-		props.put("email", "marcus.ljungblad@mah.se");
-		props.put("password", "medeamah2012");
-		User eonUser = null;
-		
-		try {
-			eonUser = userFactory.build("EonUser", "eon", props);
-		} catch (UserInitalizationException e) {
-			e.printStackTrace();
-		}
-		
-		return eonUser;
-	}
-
-	private PlatformUser createMljungblad() {
-		PlatformUser mljungblad = null;
-		try {
-			mljungblad = userService.createPlatformUser("mljungblad", "elis");
-			mljungblad.setFirstName("Marcus");
-			mljungblad.setLastName("Ljungblad");
-			mljungblad.setEmail("marcus@ljungblad.nu");
-			userService.updatePlatformUser(mljungblad);
-		} catch (UserExistsException e) {
-			e.printStackTrace();
-		} catch (NoSuchUserException e) {
-			e.printStackTrace();
-		}
-		return mljungblad;
-	}
-
 	protected void unbindUserService(UserService us) {
 		this.userService = null;
-	}
-	
-	protected void unbindUserFactory(UserFactory uf) {
-		this.userFactory = null;
 	}
 }
