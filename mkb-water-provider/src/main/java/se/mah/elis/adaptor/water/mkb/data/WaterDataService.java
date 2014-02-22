600643d49dc3df0581e78a5b696e94746cdc10e7
Marcus Ljungblad <marcus@ljungblad.nu>
2014-02-22 15:20:23 +0100

    auto reloading the data every hour

--- mkb-water-provider/src/main/java/se/mah/elis/adaptor/water/mkb/data/WaterDataService.java
+++ mkb-water-provider/src/main/java/se/mah/elis/adaptor/water/mkb/data/WaterDataService.java
@@ -4,30 +4,62 @@ import java.io.FileNotFoundException;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
+import org.joda.time.DateTime;
+import org.osgi.service.component.ComponentContext;
 
 @Component(name = "MkbWaterData", immediate = true)
 @Service(value = WaterDataService.class)
 public class WaterDataService {
-
+	
+	private Thread updaterThread;
 	private WaterData waterData;
 	
 	public WaterDataService() {
-		init();
+		updaterThread = new Thread(new UpdaterThread());
+		updaterThread.start();
+	}
+	
+	protected void deactivate(ComponentContext ctx) {
+		updaterThread.interrupt();
 	}
 	
 	public WaterDataService(WaterData data) {
 		waterData = data;
 	}
-
-	private void init() {
-		try {
-			waterData = WaterDataLoader.loadFromFile("/tmp/mkb-water-data/all.txt");
-		} catch (FileNotFoundException e) {		
-			e.printStackTrace();
-		}
-	}
 	
 	public WaterData getInstance() {
 		return waterData;
+	}
+	
+	private void setInstance(WaterData data) {
+		waterData = data;
+	}
+	
+	private class UpdaterThread implements Runnable {
+
+		private static final long ONE_HOUR = 1000 * 60 * 60;
+		
+		@Override
+		public void run() {
+			while (true) {
+				System.out.println("Reloading water data at " + DateTime.now());
+				
+				loadData();
+				
+				try {
+					Thread.sleep(ONE_HOUR);
+				} catch (InterruptedException e) {
+					break;
+				}
+			}
+		}
+		
+		private void loadData() {
+			try {
+				setInstance(WaterDataLoader.loadFromFile("/tmp/mkb-water-data/all.txt"));
+			} catch (FileNotFoundException e) {		
+				e.printStackTrace();
+			}
+		}
 	}
 }
