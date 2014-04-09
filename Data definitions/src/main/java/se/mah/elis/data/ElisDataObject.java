97b9400e930b831e318e5b643d5367cb183dc309
koddas <johan@idioti.se>
2014-04-09 04:31:42 +0200

    Removed some merge annotations.

--- Data definitions/src/main/java/se/mah/elis/data/ElisDataObject.java
+++ Data definitions/src/main/java/se/mah/elis/data/ElisDataObject.java
@@ -48,12 +48,7 @@ public interface ElisDataObject extends Serializable {
 	 * Sets the owner of this data object.
 	 * 
 	 * @param userId
-<<<<<<< HEAD
 	 * @since 2.0
-=======
-	 * @deprecated 
-	 * @since 1.0
->>>>>>> branch 'master' of https://github.com/medeamalmo/elis-platform.git
 	 */
 	void setOwnerId(UUID userId);
 	
@@ -63,12 +58,7 @@ public interface ElisDataObject extends Serializable {
 	 * associated with the system that provided it.
 	 * 
 	 * @return the unique user id.
-<<<<<<< HEAD
 	 * @since 2.0
-=======
-	 * @deprecated
-	 * @since 1.0
->>>>>>> branch 'master' of https://github.com/medeamalmo/elis-platform.git
 	 */
 	UUID getOwnerId();
 
